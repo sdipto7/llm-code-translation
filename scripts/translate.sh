ad2070dcd43193876efc2a817660a00567108ea2
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2025-03-24 17:16:03 -0600

    Added openai direct source to target lang translation and evaluation for avatar dataset

---
+++ scripts/translate.sh
@@ -0,0 +1,44 @@
+WORKDIR=`pwd`
+export PYTHONPATH=$WORKDIR;
+export PYTHONIOENCODING=utf-8;
+
+function prompt() {
+    echo;
+    echo "Syntax: bash scripts/translate.sh MODEL DATASET SRC_LANG TRG_LANG K P TEMPERATURE GPU_ID";
+    echo "MODEL: name of the model to use";
+    echo "DATASET: name of the dataset to use";
+    echo "SRC_LANG: source language";
+    echo "TRG_LANG: target language";
+    echo "K: top-k sampling";
+    echo "P: top-p sampling";
+    echo "TEMPERATURE: temperature for sampling";
+    echo "GPU_ID: GPU to use";
+    exit;
+}
+
+while getopts ":h" option; do
+    case $option in
+        h) # display help
+          prompt;
+    esac
+done
+
+if [[ $# < 8 ]]; then
+  prompt;
+fi
+
+MODEL=$1;
+DATASET=$2;
+SRC_LANG=$3;
+TRG_LANG=$4;
+K=$5;
+P=$6;
+TEMPERATURE=$7;
+GPU_ID=$8;
+
+# need changes based on future requirements
+if [[ $MODEL == "gpt-4o" ]]; then
+  python3 src/translation/openai_translation.py --dataset $DATASET --source_lang $SRC_LANG --target_lang $TRG_LANG --k $K --p $P --temperature $TEMPERATURE;
+else
+  echo "Model not supported";
+fi
