fa6065c55fe8bbcabdc47975bd4ff5f24ed2cd51
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2025-05-12 20:53:30 -0600

    Fixed validator issue

--- src/validator/arg_validator.py
+++ src/validator/arg_validator.py
@@ -57,7 +57,7 @@ def validate_language_pair(source_lang, target_lang):
     if source_lang in ["python", "java"] and target_lang in ["python", "java"] and source_lang == target_lang:
         raise ValueError(f"source language {source_lang} and target language {target_lang} should not be same")
 
-    loggin.info("language pair validated successfully")
+    logging.info("language pair validated successfully")
 
 def validate_translation_type(translation_type):
     logging.info("validating translation type")
@@ -71,7 +71,7 @@ def validate_translation_type(translation_type):
     logging.info("translation type validated successfully")
 
 def validate_arguments(args, is_test=False):
-    logging.info(f"validating arguments for the model {args.model}, dataset {args.dataset}, source language {args.source_lang}, target language {args.target_lang} and translation type {args.translation_type}")
+    logging.info(f"validating arguments for the model {args.model}{', dataset ' + args.dataset if not is_test else ''}, source language {args.source_lang}, target language {args.target_lang} and translation type {args.translation_type}")
 
     validate_model(args.model)
 
@@ -83,4 +83,4 @@ def validate_arguments(args, is_test=False):
     validate_language_pair(args.source_lang, args.target_lang)
     validate_translation_type(args.translation_type)
 
-    logging.info(f"arguments validated successfully with model: {args.model}, dataset: {args.dataset}, source language: {args.source_lang}, target language: {args.target_lang} and translation type: {args.translation_type}")
+    logging.info(f"arguments validated successfully with model: {args.model}{', dataset ' + args.dataset if not is_test else ''}, source language: {args.source_lang}, target language: {args.target_lang} and translation type: {args.translation_type}")
