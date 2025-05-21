a3fc5a76de0c3c1faf6abf59445900ef7fcf0902
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2025-05-21 17:29:26 -0600

    Updated gemini flash model

--- src/util/constants.py
+++ src/util/constants.py
@@ -7,7 +7,7 @@ def get_extension_map():
 def get_model_map():   
     return {
         "gpt-4o": "openai/gpt-4o",
-        "gemini-flash-2.5": "google/gemini-2.5-flash-preview",
+        "gemini-flash-2.5": "google/gemini-2.5-flash-preview-05-20",
         "llama-4-maverick": "meta-llama/llama-4-maverick:free",
         "qwen-2.5-coder": "qwen/qwen-2.5-coder-32b-instruct:free",
         "qwen-2.5": "qwen/qwen-2.5-72b-instruct:free",
