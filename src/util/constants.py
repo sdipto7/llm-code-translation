bdece5054e308f1f0a5d031caaa650b29f8c6821
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2025-09-09 17:50:40 -0600

    Added/Updated models

--- src/util/constants.py
+++ src/util/constants.py
@@ -7,10 +7,12 @@ def get_extension_map():
 def get_model_map():   
     return {
         "gpt-4o": "openai/gpt-4o",
-        "gemini-flash-2.5": "google/gemini-2.5-flash-preview-05-20",
+        "claude-sonnet-4": "anthropic/claude-sonnet-4",
+        "gemini-flash-2.5": "google/gemini-2.5-flash",
         "llama-4-maverick": "meta-llama/llama-4-maverick:free",
+        "qwen-3-coder": "qwen/qwen3-coder:free",
         "qwen-2.5-coder": "qwen/qwen-2.5-coder-32b-instruct:free",
         "qwen-2.5": "qwen/qwen-2.5-72b-instruct:free",
         "deepseek-r1": "deepseek/deepseek-r1:free",
-        "deepseek-v3": "deepseek/deepseek-chat:free"
+        "deepseek-v3.1": "deepseek/deepseek-chat-v3.1:free"
     }
